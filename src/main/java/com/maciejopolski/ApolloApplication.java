057db7ba752953143f950d6ee0b2dfef7e2e7954
Maniek3000 <maniek3000@o2.pl>
2021-08-08 11:20:17 +0200

    apollo start

--- src/main/java/com/maciejopolski/ApolloApplication.java
+++ src/main/java/com/maciejopolski/ApolloApplication.java
@@ -1,4 +1,4 @@
-package com.michalszalkowski;
+package com.maciejopolski;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/maciejopolski/ApolloApplicationTests.java
+++ src/test/java/com/maciejopolski/ApolloApplicationTests.java
@@ -1,4 +1,4 @@
-package com.michalszalkowski;
+package com.maciejopolski;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
